ce893fae2f2c25b501104a645512e7b0f2ed023f
Lorenzo Rimei <126676473+Riimoo30@users.noreply.github.com>
2023-04-14 10:17:05 +0200

    Update Exercise 2.kt

--- Exercise 2.kt
+++ Exercise 2.kt
@@ -2,16 +2,21 @@ package Kotlin
 
 fun main() {
     val test = Car("Audi", "TT")
+    val test2 = Motorbike("Ducati", "Monster")
     println(test.name())
     println(test.name2())
+    println(test2.name())
+    println(test2.name2())
 }
 
 class Car(override val model : String, override val brand: String) : Named, Named2(brand) {
 
 }
+class Motorbike(override val model: String, override val brand: String): Named, Named2(brand)
 
 interface Named {
     val model :String
+    
     fun name(): String{
         return "Model: $model, Using Interface"
     }
